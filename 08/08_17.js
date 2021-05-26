34eae006911f117bf277ee96b39518a05ecf0b4b
illinia <67290425+illinia@users.noreply.github.com>
2021-05-26 12:43:09 +0300

    first commit

---
+++ 08/08_17.js
@@ -0,0 +1,7 @@
+// 반복문
+var count = 0;
+
+do {
+  console.log(count);
+  count++;
+} while (count < 3);
---
+++ 08/08_20.js
@@ -0,0 +1,8 @@
+// break 문
+foo: {
+  console.log(1);
+  break foo;
+  console.log(2);
+}
+
+console.log('Done!');
